fd02b1ca422d3ed943bd9c4d44bb3e12bd6f46d9
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-10-30 21:06:52 +0530

    Reverse a number

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+package Day6LogicalPrograms;
+import	java.util.Scanner;
+public class Reverse_Num {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+        int k = num;
+        int rev = 0;
+        while( num != 0)
+        {
+            rev = rev * 10 + (num % 10);
+            num = num / 10;
+        }
+        System.out.println(rev);
+    }
+}
